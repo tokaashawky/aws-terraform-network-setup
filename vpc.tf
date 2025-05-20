d01e5741db1051395dfe10a26681642d91cc7d3d
Toka Shawky Elmaadawy <119989493+tokaashawky@users.noreply.github.com>
2025-05-20 04:02:43 +0300

    vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "toka" {   # "MyVPC" is ref to your vpc
+resource "aws_vpc" "MyVPC" {   # "MyVPC" is ref to your vpc
   cidr_block = "10.0.0.0/16"
    tags = {
     Name = "MyVPC"            # the VPC name in aws
